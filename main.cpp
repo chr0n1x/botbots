7d912d967dd3c0d3feba00dd6ecc55e79607d64c
chr0n1x <heilong24@gmail.com>
2012-04-24 18:27:05 -0400

    main can now take an int as an argument for the grid dimensions

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include "grid.hpp"
 
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
-int main(void) {
-  grid::the_grid g;
+int main(int argc, char ** argv) {
+
+  int grid_dim = 10;
+  if(argc > 1) {
+    grid_dim = atoi(argv[1]);
+  }
+  grid::the_grid g(grid_dim);
   cout << "Grid cells: " << g.cell_count() << endl;
 
+  cout << "Randomly populating grid with botbots..." << endl << endl;
   int count = 0;
   while(g.create_botizen()) {
     ++count;
