a3b0c5f757d17e019fb303da4393aa495bddc11f
chr0n1x <heilong24@gmail.com>
2012-05-01 18:40:29 -0400

    skeleton for the cortex namespace / classes

---
+++ cortex.hpp
@@ -0,0 +1,18 @@
+#ifndef __CORTEX
+#define __CORTEX
+
+namespace the_cortex {
+
+  template <typename _obj, typename _param>
+  class cortex_object {
+
+  };
+
+  template <typename _obj, typename _param>
+  class cortex {
+
+  };
+
+}
+
+#endif
