41036cec66141e153c3132444daa98247b2b1908
chr0n1x <heilong24@gmail.com>
2012-05-07 10:25:51 -0400

    comments to remind me why the internals include certain libs

--- internals.hpp
+++ internals.hpp
@@ -1,13 +1,20 @@
 #ifndef __INTERNALHEADERS
 #define __INTERNALHEADERS
 
+// strings, streams and manipulators
 #include <iostream>
 #include <iomanip>
 #include <sstream>
 #include <string>
+
+// utility functions
 #include <cstdio>
 #include <cstdlib>
+
+// for rand()
 #include <ctime>
+
+// threads
 #include <pthread.h>
 #include <unistd.h>
 
